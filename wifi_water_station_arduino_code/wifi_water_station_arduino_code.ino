33b503ebcf219c9fb876cff58b3e4c982e75bf85
amitzri3 <61781115+amitzri3@users.noreply.github.com>
2021-10-24 17:32:41 +0300

    added station code

--- wifi_water_station_arduino_code/wifi_water_station_arduino_code.ino
+++ wifi_water_station_arduino_code/wifi_water_station_arduino_code.ino
@@ -7,98 +7,53 @@
 #define FIREBASE_HOST "car-project-49490-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "ieGIVK3SFXpOGp3OYNsZyNPdZg1YAcjvQTFsWIqB"
 
-SoftwareSerial NodeMCU(TX,RX);
+#define READY_STATUS_STRING "ready"
 
-String currentServerInput = "";
 String outputForArduino = "";
-
-const char pathDelimiter[2] = "*";
 const char actionDelimiter[2] = " ";
+String Status = "shlukkkkk";
+
 
 void setup()
 {
+  delay(1000);
   Serial.begin(9600);
-  NodeMCU.begin(9600);
-  
   wifiConnect();
+  delay(1000);
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-//  if(Firebase.failed())
-//  {
-//    Serial.println(Firebase.error());
-//  }
-  delay(10);  
+  if (Firebase.failed()) {
+     Serial.println(Firebase.error());  
+     return;
+ }
+  delay(1000);
 }
 
+bool carInPlace = false;
 void loop()
-{
-  delay(2000);
-//  Serial.println("Wifi Loop Refresh");
-//  Firebase.setFloat("number", num); //counter for sanity check
-  String path = Firebase.getString("Path");
-  if(path == currentServerInput)
-  {
-    return;
-  }
-  currentServerInput = path;
-
-  parseAndSendCommands(path);
-  
-//  Firebase.setString("Rec", path);
-//  delay(500);
-
-  delay(10);
-}
-
-void parseAndSendCommands(String path)
-{
-  char cstr[path.length() + 1];
-  strcpy(cstr, path.c_str());
-  char* token;
-  char* action;
-  char* amount;
-  float faction;
-  float famount;
-  
-  action = strtok(cstr, actionDelimiter);
-  amount = strtok(NULL, actionDelimiter);
-  faction = atof(action);
-  famount = atof(amount);
-  
-  while(action != NULL && amount != NULL)
-  {
-    sendActionToArduino(faction, famount);
-    action = strtok(NULL, actionDelimiter);
-    if(action == NULL) return;
-    amount = strtok(NULL, actionDelimiter);
-    if(amount == NULL) return;
-    faction = atof(action);
-    famount = atof(amount);
+{  
+  Status = Firebase.getString("CarInPlace");
+  while(!Status){
+    delay(2000);
+    Status = Firebase.getString("Status");
     delay(50);
   }
+  updateStationToRefill();
 }
 
-void sendActionToArduino(float faction, float famount){
-  Serial.print(faction);
-  delay(50);
-  Serial.println("\n");
-  delay(50);
-  Serial.print(famount);
-  delay(50);
-  Serial.println("\n");
-  delay(50);
+void updateStationToRefill(){
+  Serial.println("refill");
 }
 
 void wifiConnect()
 {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // Connect to the network
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);             // Connect to the network
   //Serial.print("Connecting to ");
   //Serial.print(WIFI_SSID); Serial.println(" ...");
 
-//  int teller = 0;
+  int teller = 0;
   while (WiFi.status() != WL_CONNECTED)
-  {
-    // Wait for the Wi-Fi to connect
-    delay(500);
+  {                                       // Wait for the Wi-Fi to connect
+    delay(1000);
    // Serial.print(++teller); Serial.print(' ');
   }
 
